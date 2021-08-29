3b697c917c32580e4823d522c929528ea33b1450
eightgran <eightgran@gmail.com>
2021-08-29 14:30:15 +0200

    Add `LitCardTitle`

---
+++ lib/src/widgets/title/lit_card_title.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:leitmotif/leitmotif.dart';
+
+class LitCardTitle extends StatelessWidget {
+  final String title;
+  final String? subtitle;
+  const LitCardTitle({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          title,
+          style: LitSansSerifStyles.h6,
+        ),
+        subtitle != null ? SizedBox(height: 4.0) : SizedBox(),
+        subtitle != null
+            ? Text(
+                subtitle!.toUpperCase(),
+                style: LitSansSerifStyles.overline.copyWith(
+                  letterSpacing: 1.0,
+                ),
+              )
+            : SizedBox()
+      ],
+    );
+  }
+}
