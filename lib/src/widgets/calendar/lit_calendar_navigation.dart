941fe431cbde0743630a3bdf3a0fa93a93e04a3e
eightgran <eightgran@gmail.com>
2021-09-09 18:19:59 +0200

    Improve button sizing on `LitCalendarNavigation`

--- lib/src/widgets/calendar/lit_calendar_navigation.dart
+++ lib/src/widgets/calendar/lit_calendar_navigation.dart
@@ -20,37 +20,36 @@ class LitCalendarNavigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4.0),
+      padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 4.0),
       child: LayoutBuilder(builder: (context, constraints) {
         return Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              width: constraints.maxWidth / 4,
+              width: 46,
+              height: 42,
               //decreaseByMonth
               child: _MonthNavigatorButton(
                 icon: LitIcons.chevron_left_solid,
                 onChange: decreaseByMonth,
               ),
             ),
-            SizedBox(
-              width: constraints.maxWidth / 4,
-              child: LitPlainLabelButton(
-                fontSize: 16.0,
-                onPressed: onMonthLabelPress,
-                label: monthLabel,
+            _TimeUnitSelection(
+              constraints: BoxConstraints(
+                minWidth: constraints.minWidth,
+                maxHeight: constraints.minWidth - (46 * 2),
+                maxWidth: constraints.maxWidth - (46 * 2),
+                minHeight: constraints.minHeight,
               ),
+              monthLabel: monthLabel,
+              yearLabel: yearLabel,
+              onMonthPress: onMonthLabelPress,
+              onYearPress: onYearLabelPress,
             ),
             SizedBox(
-              width: constraints.maxWidth / 4,
-              child: LitPlainLabelButton(
-                fontSize: 16.0,
-                onPressed: onYearLabelPress,
-                label: yearLabel,
-              ),
-            ),
-            SizedBox(
-              width: constraints.maxWidth / 4,
+              width: 46,
+              height: 42,
+
               //decreaseByMonth
               child: _MonthNavigatorButton(
                 icon: LitIcons.chevron_right_solid,
@@ -60,6 +59,62 @@ class LitCalendarNavigation extends StatelessWidget {
           ],
         );
       }),
+    );
+  }
+}
+
+class _TimeUnitSelection extends StatelessWidget {
+  final BoxConstraints constraints;
+  final String monthLabel;
+  final String yearLabel;
+  final void Function() onMonthPress;
+  final void Function() onYearPress;
+  const _TimeUnitSelection({
+    Key? key,
+    required this.constraints,
+    required this.monthLabel,
+    required this.yearLabel,
+    required this.onMonthPress,
+    required this.onYearPress,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            color: LitColors.lightGrey.withOpacity(0.1),
+            child: SizedBox(
+              width: constraints.maxWidth * 0.5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: LitPlainLabelButton(
+                  fontSize: 16.0,
+                  onPressed: onMonthPress,
+                  label: monthLabel,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            color: LitColors.lightGrey.withOpacity(0.2),
+            child: SizedBox(
+              width: constraints.maxWidth * 0.5,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                child: LitPlainLabelButton(
+                  fontSize: 16.0,
+                  onPressed: onYearPress,
+                  label: yearLabel,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -77,8 +132,8 @@ class _MonthNavigatorButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return LitPushedThroughButton(
       padding: const EdgeInsets.symmetric(
-        vertical: 8.0,
-        horizontal: 12.0,
+        vertical: 4.0,
+        horizontal: 2.0,
       ),
       onPressed: onChange,
       child: FittedBox(
@@ -89,8 +144,8 @@ class _MonthNavigatorButton extends StatelessWidget {
           color: LitColors.mediumGrey,
         ),
       ),
-      accentColor: Color(0xFF919191),
-      backgroundColor: Color(0xFFeeeeee),
+      // accentColor: Color(0xFF919191),
+      // backgroundColor: Color(0xFFeeeeee),
     );
   }
 }
