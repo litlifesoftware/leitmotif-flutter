64da52e06b69c19f923835a273e92baf65baa6e8
eightgran <eightgran@gmail.com>
2021-09-14 19:41:20 +0200

    Refactor `LitTitledActionCard`

--- lib/src/widgets/containers/lit_titled_action_card.dart
+++ lib/src/widgets/containers/lit_titled_action_card.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:leitmotif/leitmotif.dart';
 
-///
+/// A Flutter widget displaying information and allowing the user to pick an
+/// action to respond to the given information.
 class LitTitledActionCard extends StatelessWidget {
+  /// The card's title.
   final String title;
+
+  /// The card's subtitle. None displayed if none provided.
   final String? subtitle;
+
+  /// The card's inner margin.
+  final EdgeInsets margin;
+
+  /// The card's background colors.
+  final List<Color> backgroundColors;
+
+  /// The card's child widget.
   final Widget child;
+
+  /// The action button's data. Action buttons are displayed on the bottom of
+  /// the card.
   final List<ActionButtonData>? actionButtonData;
+
+  /// Creates a [LitTitledActionCard].
   const LitTitledActionCard({
     Key? key,
     required this.title,
     this.subtitle,
+    this.margin = const EdgeInsets.symmetric(
+      vertical: 16.0,
+      horizontal: 16.0,
+    ),
+    this.backgroundColors = const [Colors.white, Colors.white],
     this.child = const SizedBox(),
     this.actionButtonData,
   }) : super(key: key);
@@ -22,12 +44,9 @@ class LitTitledActionCard extends StatelessWidget {
         return Column(
           children: [
             LitGradientCard(
-              colors: [Colors.white, Colors.white],
+              colors: backgroundColors,
               child: Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: 16.0,
-                  horizontal: 16.0,
-                ),
+                padding: margin,
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
@@ -53,9 +72,11 @@ class LitTitledActionCard extends StatelessWidget {
 class _ButtonBuilder extends StatelessWidget {
   final List<ActionButtonData>? actionButtonData;
   final BoxConstraints constraints;
+  final EdgeInsets margin;
   const _ButtonBuilder({
     Key? key,
     required this.actionButtonData,
+    this.margin = const EdgeInsets.only(top: 16.0),
     required this.constraints,
   }) : super(key: key);
 
@@ -65,7 +86,6 @@ class _ButtonBuilder extends StatelessWidget {
       builder: (context) {
         List<Widget> children = [];
         for (ActionButtonData actionButton in actionButtonData!) {
-          children.add(SizedBox(height: 24.0));
           children.add(
             SizedBox(
               width: constraints.maxWidth,
@@ -82,8 +102,11 @@ class _ButtonBuilder extends StatelessWidget {
             ),
           );
         }
-        return Column(
-          children: children,
+        return Padding(
+          padding: margin,
+          child: Column(
+            children: children,
+          ),
         );
       },
     );
