61c7c68d161d1c8d715d4f4c11b9987fee8fbc7a
eightgran <eightgran@gmail.com>
2021-09-25 17:39:13 +0200

    Add `xs` option on `LitBoxShadows`

--- lib/src/styles/lit_box_shadows.dart
+++ lib/src/styles/lit_box_shadows.dart
@@ -3,6 +3,16 @@ import 'package:flutter/material.dart';
 /// A collection of [BoxShadow] objects that can be used as decoration.
 class LitBoxShadows {
   /// A [BoxShadow] that can be applied on smaller elements.
+  static const List<BoxShadow> xs = const [
+    const BoxShadow(
+      offset: Offset(-1.0, 1.0),
+      blurRadius: 3.0,
+      color: Colors.black26,
+      spreadRadius: -1.0,
+    )
+  ];
+
+  /// A [BoxShadow] that can be applied on smaller elements.
   static const List<BoxShadow> sm = const [
     const BoxShadow(
       offset: Offset(-2.0, 2.0),
