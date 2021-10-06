03517a2e5292a817049adcf667872dae16e33d9e
eightgran <eightgran@gmail.com>
2021-10-06 20:30:57 +0200

    Remove `calendar` library

--- lib/calendar.dart
+++
@@ -1,9 +0,0 @@
-/// Flutter widgets implementing the custom calendar view.
-///
-/// To use, import `package:leitmotif/calendar.dart`.
-library calendar;
-
-export 'src/widgets/calendar/lit_calendar_day_item.dart';
-export 'src/widgets/calendar/lit_calendar_grid.dart';
-export 'src/widgets/calendar/lit_calendar_navigation.dart';
-export 'src/widgets/calendar/lit_calendar_weekday_label.dart';
