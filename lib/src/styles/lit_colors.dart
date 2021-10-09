fd619f8e84b4465d14e233250452b7a0e5a62dd9
eightgran <eightgran@gmail.com>
2021-10-09 18:13:44 +0200

    Refactor `LitColors`

--- lib/src/styles/lit_colors.dart
+++ lib/src/styles/lit_colors.dart
@@ -1,26 +1,54 @@
 import 'package:flutter/material.dart';
 
-/// A collection of [Color]s found throughout the LitLifeSoftware apps.
-///
-/// The [Color]s are able to be used as default value on the constructor
-/// if required.
+/// A collection of [Color] objects implementing the `Leitmotif` color palette.
 class LitColors {
-  static const Color lightRed = Color(0xFFD4ABB6);
-  static const Color midRed = Color(0xFFD66171);
-  static const Color darkRed = Color(0xFFEE3D6D);
-  static const Color lightPink = Color(0xFFE3C3C4);
-  static const Color lightGreen = Color(0xFFC6F0BD);
-  static const Color lightBlue = Color(0xFF97BFCA);
-  static const Color darkBlue = Color(0xFF1D2027);
-  static const Color darkGreen = Color(0xFFA4C79D);
-  static const Color mintGreen = Color(0xFFBDD7D9);
-  static const Color lightMintGreen = Color(0xFFCEF0EB);
-  static const Color cyan = Color(0xFF59E3C3);
-  static const Color lightGrey = Color(0xFFC7C7C7);
-  static const Color mediumGrey = Color(0xFF686868);
-  static const Color darkGrey = Color(0xFF444444);
-  static const Color darkBeige = Color(0xFFB3958C);
-  static const Color beigeGrey = Color(0xFFBAB3AF);
-  static const Color mediumOliveGreen = Color(0xFF9AA390);
-  static const Color darkOliveGreen = Color(0xFF4A5248);
+  //TODO: REMOVE
+  static const Color lightRed = red400;
+  static const Color midRed = red600;
+  static const Color darkRed = red700;
+  static const Color lightPink = red200;
+  static const Color lightGreen = green300;
+  static const Color lightBlue = blue300;
+  static const Color darkBlue = grey600;
+  static const Color darkGreen = green300;
+  static const Color mintGreen = green200;
+  static const Color lightMintGreen = green100;
+  static const Color cyan = blue400;
+  static const Color lightGrey = grey200;
+  static const Color mediumGrey = grey400;
+  static const Color darkGrey = grey500;
+  static const Color darkBeige = red500;
+  static const Color beigeGrey = red100;
+  static const Color mediumOliveGreen = green500;
+  static const Color darkOliveGreen = green600;
+
+  ///
+  ///
+  static const Color grey50 = const Color(0xFFFDFDFD);
+  static const Color grey100 = const Color(0xFFf4f4f7);
+  static const Color grey150 = const Color(0xFFDEDEDE);
+  static const Color grey200 = const Color(0xFFC7C7C7);
+  static const Color grey300 = const Color(0xFFD1CDCD);
+  static const Color grey400 = const Color(0xFF686868);
+  static const Color grey500 = const Color(0xFF444444);
+  static const Color grey600 = const Color(0xFF1D2027);
+
+  static const Color red100 = const Color(0xFFBAB3AF);
+  static const Color red200 = const Color(0xFFE3C3C4);
+  static const Color red400 = const Color(0xFFD4ABB6);
+  static const Color red500 = const Color(0xFFB3958C);
+  static const Color red600 = const Color(0xFFD66171);
+  static const Color red700 = const Color(0xFFEE3D6D);
+
+  static const Color green100 = const Color(0xFFCEF0EB);
+  static const Color green200 = const Color(0xFFBDD7D9);
+  static const Color green300 = const Color(0xFFC6F0BD);
+  static const Color green400 = const Color(0xFFA4C79D);
+  static const Color green500 = const Color(0xFF9AA390);
+  static const Color green600 = const Color(0xFF4A5248);
+
+  static const Color blue300 = const Color(0xFF97BFCA);
+  static const Color blue400 = const Color(0xFF59E3C3);
+
+  ///
 }
