5c318f2ff61746e54c2a38fe202dff443ecf6a8b
eightgran <eightgran@gmail.com>
2021-10-11 20:15:35 +0200

    Add gradients to `LitGradients`

--- lib/src/styles/lit_gradients.dart
+++ lib/src/styles/lit_gradients.dart
@@ -1,15 +1,45 @@
 import 'package:flutter/painting.dart';
+import 'package:leitmotif/styles.dart';
 
 /// A collection of [LinearGradient] that can be used as decoration.
 
 class LitGradients {
+  /// A light grey gradient.
+  static const LinearGradient lightGreyGradient = const LinearGradient(
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+    stops: [0.65, 1.00],
+    colors: [
+      LitColors.grey100,
+      LitColors.grey300,
+    ],
+  );
+
   /// A grey linear gradient.
   static const LinearGradient greyGradient = LinearGradient(
     begin: Alignment.topRight,
     end: Alignment.bottomLeft,
     colors: const [
-      const Color(0xFFFDFDFD),
-      const Color(0xFFDEDEDE),
+      LitColors.grey50,
+      LitColors.grey150,
+    ],
+  );
+
+  static const LinearGradient dark = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: const [
+      LitColors.mediumGrey,
+      LitColors.darkBlue,
+    ],
+  );
+
+  static const LinearGradient pinkWhite = LinearGradient(
+    begin: Alignment.topRight,
+    end: Alignment.bottomLeft,
+    colors: const [
+      LitColors.lightGrey,
+      LitColors.lightPink,
     ],
   );
 }
