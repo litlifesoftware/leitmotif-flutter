d70e065eadbcd1f0e0ef1521ff02a58c82ba98d3
eightgran <eightgran@gmail.com>
2021-10-17 21:02:36 +0200

    Document `LitNavigationButton`

--- lib/src/widgets/buttons/lit_navigation_button.dart
+++ lib/src/widgets/buttons/lit_navigation_button.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:leitmotif/leitmotif.dart';
 
-//TODO: Document
+/// A Leitmotif `button` widget allowing to either navigate back or forth on a
+/// calendar view.
 class LitNavigationButton extends StatelessWidget {
   final LitCalendarNavigationMode mode;
   final EdgeInsets padding;
   final Color backgroundAccentColor;
   final void Function() onPressed;
 
+  /// Creates a [LitNavigationButton].
   const LitNavigationButton({
     Key? key,
     required this.mode,
@@ -15,10 +17,12 @@ class LitNavigationButton extends StatelessWidget {
       vertical: 4.0,
       horizontal: 2.0,
     ),
-    this.backgroundAccentColor = const Color(0xFFfbfbfb),
+    this.backgroundAccentColor = LitColors.grey50,
     required this.onPressed,
   }) : super(key: key);
 
+  /// Returns an icon according to the provided [LitCalendarNavigationMode]
+  /// value.
   IconData get _icon {
     switch (mode) {
       case LitCalendarNavigationMode.next:
