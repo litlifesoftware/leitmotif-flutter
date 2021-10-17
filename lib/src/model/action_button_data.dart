9a0aa6405216fff10fd46a85e14e4d21ddbc0e5c
eightgran <eightgran@gmail.com>
2021-10-17 18:52:43 +0200

    Add default values on `ActionButtonData`

--- lib/src/model/action_button_data.dart
+++ lib/src/model/action_button_data.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:leitmotif/styles.dart';
 
-///
+/// A Leitmotif `model` class storing data to provide required to build buttons.
 class ActionButtonData {
   final String title;
+  final Color accentColor;
+  final Color backgroundColor;
   final void Function() onPressed;
-  final Color? backgroundColor;
-  final Color? accentColor;
 
+  /// Creates a [ActionButtonData].
   const ActionButtonData({
     required this.title,
+    this.accentColor = Colors.white,
+    this.backgroundColor = LitColors.grey50,
     required this.onPressed,
-    required this.backgroundColor,
-    required this.accentColor,
   });
 }
