903e971e93388cce08e91f68fd245bbecce8d229
eightgran <eightgran@gmail.com>
2021-10-23 15:56:19 +0200

    Refactor `LitDescriptionTextBox`

--- lib/src/widgets/containers/lit_description_box.dart
+++ lib/src/widgets/containers/lit_description_box.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:leitmotif/leitmotif.dart';
 
-const double _ICON_CONTAINER_WIDTH = 42.0;
-const double _ICON_SIZE = 13.0;
-const int _ICON_COLOR_DARK = 0xFF616161;
-const int _ICON_COLOR_LIGHT = 0xFFFFFFFf;
-const EdgeInsets _ICON_PADDING = const EdgeInsets.only(top: 8.0, bottom: 8.0);
-const EdgeInsets _TEXT_BOX_PADDING = const EdgeInsets.only(
-  right: 4.0,
-  left: 8.0,
-  top: 8.0,
-  bottom: 8.0,
-);
-
-/// A Flutter widget displaying an [Icon] alongside the provided description
-/// text in order to share information in a textual manner.
+/// A Leitmotif `containers` widget allowing to display information alongside
+/// an icon.
 class LitDescriptionTextBox extends StatefulWidget {
+  /// The icon.
   final IconData icon;
+
+  /// The title.
   final String? title;
+
+  /// The description text body.
   final String text;
+
+  /// The outer padding.
   final EdgeInsets padding;
 
+  /// The maximum number of lines the description text body should have.
   final int maxLines;
+
+  /// The icon container's background color.
   final Color iconBackgroundColor;
+
+  /// The text style.
   final TextStyle style;
 
   /// Creates a [LitDescriptionTextBox].
@@ -39,14 +39,27 @@ class LitDescriptionTextBox extends StatefulWidget {
 
   @override
   _LitDescriptionTextBoxState createState() => _LitDescriptionTextBoxState();
+
+  static const double _iconContainerWidth = 42.0;
+  static const double _iconSize = 13.0;
+  static const int _colorDark = 0xFF616161;
+  static const int _colorLight = 0xFFFFFFFf;
+  static const EdgeInsets _iconPadding =
+      const EdgeInsets.only(top: 8.0, bottom: 8.0);
+  static const EdgeInsets _textBoxPadding = const EdgeInsets.only(
+    right: 4.0,
+    left: 8.0,
+    top: 8.0,
+    bottom: 8.0,
+  );
 }
 
 class _LitDescriptionTextBoxState extends State<LitDescriptionTextBox> {
   /// Either returns the dark or light text color.
   Color get _iconColor {
     return widget.iconBackgroundColor.applyColorByContrast(
-      Color(_ICON_COLOR_LIGHT),
-      Color(_ICON_COLOR_DARK),
+      Color(LitDescriptionTextBox._colorLight),
+      Color(LitDescriptionTextBox._colorDark),
     );
   }
 
@@ -62,61 +75,22 @@ class _LitDescriptionTextBoxState extends State<LitDescriptionTextBox> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 // Icon container.
-                Padding(
-                  padding: _ICON_PADDING,
-                  child: Container(
-                    height: _ICON_CONTAINER_WIDTH,
-                    width: _ICON_CONTAINER_WIDTH,
-                    decoration: BoxDecoration(
-                      color: widget.iconBackgroundColor,
-                      boxShadow: LitBoxShadows.sm,
-                      borderRadius: BorderRadius.circular(
-                        16.0,
-                      ),
-                    ),
-                    child: Center(
-                      child: Icon(
-                        widget.icon,
-                        size: _ICON_SIZE,
-                        color: _iconColor,
-                      ),
-                    ),
-                  ),
+                _IconContainer(
+                  icon: widget.icon,
+                  iconBackgroundColor: widget.iconBackgroundColor,
+                  iconColor: _iconColor,
                 ),
-
                 // Text box.
                 Container(
-                  width: constraints.maxWidth - _ICON_CONTAINER_WIDTH,
+                  width: constraints.maxWidth -
+                      LitDescriptionTextBox._iconContainerWidth,
                   child: Padding(
-                    padding: _TEXT_BOX_PADDING,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: widget.title != null
-                          ? MainAxisAlignment.start
-                          : MainAxisAlignment.center,
-                      children: [
-                        widget.title != null
-                            ? ClippedText(
-                                widget.title!.toUpperCase(),
-                                maxLines: 1,
-                                style: widget.style.copyWith(
-                                  fontWeight: FontWeight.lerp(
-                                    widget.style.fontWeight,
-                                    FontWeight.bold,
-                                    0.75,
-                                  ),
-                                ),
-                              )
-                            : SizedBox(),
-                        SizedBox(
-                          height: widget.title != null ? 2.0 : 0.0,
-                        ),
-                        ClippedText(
-                          widget.text,
-                          maxLines: widget.maxLines,
-                          style: widget.style,
-                        ),
-                      ],
+                    padding: LitDescriptionTextBox._textBoxPadding,
+                    child: _TextBox(
+                      maxLines: widget.maxLines,
+                      style: widget.style,
+                      text: widget.text,
+                      title: widget.title,
                     ),
                   ),
                 ),
@@ -125,6 +99,89 @@ class _LitDescriptionTextBoxState extends State<LitDescriptionTextBox> {
           );
         },
       ),
+    );
+  }
+}
+
+/// A widget to display an icon inside a decorated container.
+class _IconContainer extends StatelessWidget {
+  final IconData icon;
+  final Color iconBackgroundColor;
+  final Color iconColor;
+  const _IconContainer({
+    Key? key,
+    required this.icon,
+    required this.iconBackgroundColor,
+    required this.iconColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: LitDescriptionTextBox._iconPadding,
+      child: Container(
+        height: LitDescriptionTextBox._iconContainerWidth,
+        width: LitDescriptionTextBox._iconContainerWidth,
+        decoration: BoxDecoration(
+          color: iconBackgroundColor,
+          boxShadow: LitBoxShadows.sm,
+          borderRadius: BorderRadius.circular(
+            16.0,
+          ),
+        ),
+        child: Center(
+          child: Icon(
+            icon,
+            size: LitDescriptionTextBox._iconSize,
+            color: iconColor,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _TextBox extends StatelessWidget {
+  final String? title;
+  final String text;
+  final int maxLines;
+  final TextStyle style;
+  const _TextBox({
+    Key? key,
+    required this.title,
+    required this.text,
+    required this.maxLines,
+    required this.style,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        title != null
+            ? ClippedText(
+                title!.toUpperCase(),
+                maxLines: 1,
+                style: style.copyWith(
+                  fontWeight: FontWeight.lerp(
+                    style.fontWeight,
+                    FontWeight.bold,
+                    0.75,
+                  ),
+                ),
+              )
+            : SizedBox(height: 0),
+        SizedBox(
+          height: title != null ? 2.0 : 0.0,
+        ),
+        ClippedText(
+          text,
+          maxLines: maxLines,
+          style: style,
+        ),
+      ],
     );
   }
 }
