e69a38539c0eecdd44db63cc2e8444b120b2dcf5
eightgran <eightgran@gmail.com>
2021-10-23 16:33:01 +0200

    Remove deprecated `buttons` widgets

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,21 +1,17 @@
-/// Flutter widgets implementing custom styled buttons.
+/// Leitmotif widgets implementing custom-styled buttons.
 ///
 /// To use, import `package:leitmotif/buttons.dart`.
 library buttons;
 
-export 'src/widgets/buttons/lit_tooltip_button.dart';
 export 'src/widgets/buttons/lit_back_button.dart';
 export 'src/widgets/buttons/circular_close_button.dart';
 export 'src/widgets/buttons/lit_transform_animated_button.dart';
-export 'src/widgets/buttons/lit_rounded_outlined_button.dart';
-export 'src/widgets/buttons/lit_rounded_flat_button.dart';
 export 'src/widgets/buttons/lit_rounded_elevated_button.dart';
 export 'src/widgets/buttons/lit_toggle_button.dart';
 export 'src/widgets/buttons/lit_switch_button.dart';
 export 'src/widgets/buttons/lit_pushed_button.dart';
 export 'src/widgets/buttons/lit_pushed_through_button.dart';
 export 'src/widgets/buttons/lit_gradient_button.dart';
-export 'src/widgets/buttons/dialog_back_button.dart';
 export 'src/widgets/buttons/lit_bubble_button.dart';
 export 'src/widgets/buttons/lit_glowing_button.dart';
 export 'src/widgets/buttons/lit_plain_label_button.dart';
