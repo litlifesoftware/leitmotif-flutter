153771264924ad3d2cd84fab827c77f4d1a13b48
eightgran <eightgran@gmail.com>
2021-11-07 16:07:23 +0100

    Remove deprecated widgets - Reorganize library exports

--- lib/containers.dart
+++ lib/containers.dart
@@ -4,22 +4,22 @@
 /// To use, import `package:leitmotif/containers.dart`.
 library containers;
 
-export 'src/widgets/containers/fade_in_transform_container.dart';
-export 'src/widgets/containers/fade_in_transform_scale_container.dart';
 export 'src/widgets/containers/blured_background_container.dart';
-export 'src/widgets/containers/scrollable_column.dart';
+export 'src/widgets/containers/clean_ink_well.dart';
+export 'src/widgets/containers/fade_in_transform_container.dart';
+export 'src/widgets/containers/indexed_page_view.dart';
 export 'src/widgets/containers/lit_constrained_size_box.dart';
 export 'src/widgets/containers/lit_description_box.dart';
 export 'src/widgets/containers/lit_elevated_card.dart';
+export 'src/widgets/containers/lit_elevated_glass_card.dart';
+export 'src/widgets/containers/lit_draggable.dart';
 export 'src/widgets/containers/lit_gradient_card.dart';
 export 'src/widgets/containers/lit_notification_container.dart';
-export 'src/widgets/containers/lit_elevated_glass_card.dart';
-export 'src/widgets/backgrounds/lit_animated_gradient_background.dart';
-export 'src/widgets/containers/lit_draggable.dart';
-export 'src/widgets/containers/lit_footer.dart';
 export 'src/widgets/containers/lit_setting_card.dart';
 export 'src/widgets/containers/lit_settings_footer.dart';
-export 'src/widgets/containers/clean_ink_well.dart';
-export 'src/widgets/containers/indexed_page_view.dart';
-export 'src/widgets/containers/lit_divider.dart';
+export 'src/widgets/containers/lit_footer.dart';
 export 'src/widgets/containers/lit_titled_action_card.dart';
+export 'src/widgets/containers/scrollable_column.dart';
+//TODO Move to art library ...
+export 'src/widgets/backgrounds/lit_animated_gradient_background.dart';
+export 'src/widgets/containers/lit_divider.dart';
--- lib/screens.dart
+++ lib/screens.dart
@@ -4,15 +4,14 @@
 /// To use, import `package:leitmotif/screens.dart`.
 library screens;
 
-export 'src/screens/title_screen.dart';
 export 'src/screens/application_licenses_screen.dart';
 export 'src/screens/application_licence_details_screen.dart';
-export 'src/screens/lit_privacy_policy_screen.dart';
-export 'src/screens/lit_onboarding_screen.dart';
 export 'src/screens/lit_credits_screen.dart';
-export 'src/screens/lit_sign_up_screen.dart';
-export 'src/screens/photosensitive_disclaimer_screen.dart';
-export 'src/screens/lit_verify_age_screen.dart';
-export 'src/screens/lit_startup_screen.dart';
+export 'src/screens/lit_onboarding_screen.dart';
 export 'src/screens/lit_privacy_disclaimer_screen.dart';
+export 'src/screens/lit_privacy_policy_screen.dart';
+export 'src/screens/lit_sign_up_screen.dart';
+export 'src/screens/lit_startup_screen.dart';
 export 'src/screens/lit_static_loading_screen.dart';
+export 'src/screens/lit_verify_age_screen.dart';
+export 'src/screens/photosensitive_disclaimer_screen.dart';
