947cf73d83a55e2b4c0093cb43f254781a8a716c
eightgran <eightgran@gmail.com>
2021-11-13 14:23:06 +0100

    Refactor `LitGlowingButton` - Adjust default colors - Add documentation

--- lib/src/widgets/buttons/lit_glowing_button.dart
+++ lib/src/widgets/buttons/lit_glowing_button.dart
@@ -1,27 +1,64 @@
 import 'package:flutter/material.dart';
 import 'package:leitmotif/leitmotif.dart';
 
+/// A Leitmotif `buttons` widget displaying a custom-styled action button.
+///
+/// The default styling will create a pink-glow effect on a pink-colored
+/// background.
 class LitGlowingButton extends StatelessWidget {
-  final void Function() onPressed;
-  final Color baseColor;
+  /// The button's background color.
+  final Color backgroundColor;
+
+  /// The button's accent color.
   final Color accentColor;
-  final Widget child;
-  final EdgeInsets padding;
+
+  /// The button's inner margin.
+  final EdgeInsets margin;
+
+  /// The button's border radius.
   final BorderRadius borderRadius;
-  final Offset offset;
+
+  /// The button's box shadow offset.
+  final Offset boxOffset;
+
+  /// The button's child widget.
+  final Widget child;
+
+  /// Handles the `onPressed` action.
+  final void Function() onPressed;
+
+  /// Creates a [LitGlowingButton].
   const LitGlowingButton({
     Key? key,
     required this.onPressed,
-    this.baseColor = const Color(0xFFef93a1),
-    this.accentColor = const Color(0xFFb2b2b2),
+    this.backgroundColor = LitColors.red580,
+    this.accentColor = LitColors.grey380,
     required this.child,
-    this.padding = const EdgeInsets.symmetric(
-      vertical: 6.0,
-      horizontal: 22.0,
-    ),
-    this.borderRadius = const BorderRadius.all(Radius.circular(16.0)),
-    this.offset = const Offset(2, 2),
+    this.margin = LitEdgeInsets.button,
+    this.borderRadius = LitBorderRadius.button,
+    this.boxOffset = const Offset(2.0, 2.0),
   }) : super(key: key);
+
+  /// Returns an interpolated background color based on the provided base
+  /// background color and the [accentColor].
+  Color? get _lerpedBackground => Color.lerp(
+        backgroundColor,
+        accentColor,
+        0.4,
+      );
+
+  BoxShadow get _base => BoxShadow(
+        blurRadius: 8.0,
+        offset: Offset(-boxOffset.dx, -boxOffset.dy),
+        color: _lerpedBackground!.withOpacity(0.4),
+        spreadRadius: 1.0,
+      );
+  BoxShadow get _accent => BoxShadow(
+        blurRadius: 8.0,
+        offset: boxOffset,
+        color: _lerpedBackground!.withOpacity(0.7),
+        spreadRadius: 1.0,
+      );
   @override
   Widget build(BuildContext context) {
     return LitPushedButton(
@@ -29,34 +66,19 @@ class LitGlowingButton extends StatelessWidget {
       child: Container(
         decoration: BoxDecoration(
           boxShadow: [
-            BoxShadow(
-              blurRadius: 8.0,
-              offset: Offset(-offset.dx, -offset.dy),
-              color: Color.lerp(baseColor, accentColor, 0.7)!.withOpacity(0.4),
-              spreadRadius: 1.0,
-            ),
-            BoxShadow(
-              blurRadius: 8.0,
-              offset: offset,
-              color: Color.lerp(baseColor, accentColor, 0.4)!.withOpacity(0.4),
-              spreadRadius: 1.0,
-            ),
+            _base,
+            _accent,
           ],
           gradient: LinearGradient(
-              begin: Alignment.bottomLeft,
-              end: Alignment.topRight,
-              stops: [
-                0.0,
-                0.95,
-              ],
-              colors: [
-                baseColor,
-                accentColor
-              ]),
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+            stops: [0.0, 0.95],
+            colors: [backgroundColor, accentColor],
+          ),
           borderRadius: borderRadius,
         ),
         child: Padding(
-          padding: padding,
+          padding: margin,
           child: child,
         ),
       ),
