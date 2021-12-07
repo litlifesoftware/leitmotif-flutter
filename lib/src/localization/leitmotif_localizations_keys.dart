994ca46dd91024129e9dbdaccb620c1883d7b785
eightgran <eightgran@gmail.com>
2021-12-07 20:41:05 +0100

    Add `LeitmotifLocalizationsKeys` - Seperated from LeitmotifLocalizations

---
+++ lib/src/localization/leitmotif_localizations_keys.dart
@@ -0,0 +1,68 @@
+/// Contains all keys that map to a localized value.
+///
+/// The keys on the localization map are accessed using it's corresponding
+/// [LeitmotifLocalizationsKeys] property to enforce integrity.
+class LeitmotifLocalizationsKeys {
+  const LeitmotifLocalizationsKeys();
+  static const String submitLabel = 'submitLabel';
+  static const String applyLabel = 'applyLabel';
+  static const String cancelLabel = 'cancelLabel';
+  static const String startLabel = 'startLabel';
+  static const String selectLabel = 'selectLabel';
+  static const String resetLabel = 'resetLabel';
+  static const String backLabel = 'backLabel';
+  static const String dateInputLabel = 'dateInputLabel';
+  static const String colorInputLabel = 'colorInputLabel';
+  static const String ageInputLabel = 'ageInputLabel';
+  static const String ageRequiredLabel = 'ageRequiredLabel';
+  static const String dayOfWeek1 = 'dayOfWeek1';
+  static const String dayOfWeek2 = 'dayOfWeek2';
+  static const String dayOfWeek3 = 'dayOfWeek3';
+  static const String dayOfWeek4 = 'dayOfWeek4';
+  static const String dayOfWeek5 = 'dayOfWeek5';
+  static const String dayOfWeek6 = 'dayOfWeek6';
+  static const String dayOfWeek7 = 'dayOfWeek7';
+  static const String january = 'january';
+  static const String february = 'february';
+  static const String march = 'march';
+  static const String april = 'april';
+  static const String may = 'may';
+  static const String june = 'june';
+  static const String july = 'july';
+  static const String august = 'august';
+  static const String september = 'september';
+  static const String october = 'october';
+  static const String november = 'november';
+  static const String december = 'december';
+  static const String yourAgeLabel = 'yourAgeLabel';
+  static const String invalidInputLabel = 'invalidInputLabel';
+  static const String invalidInputBody = 'invalidInputBody';
+  static const String invalidInputExtendedBody = 'invalidInputExtendedBody';
+  static const String verifyAgeScreenSuccessTitle =
+      'verifyAgeScreenSuccessTitle';
+  static const String verifyAgeScreenSuccessSubtitle =
+      'verifyAgeScreenSuccessSubtitle';
+  static const String verifyAgeScreenSuccessBody = 'verifyAgeScreenSuccessBody';
+  static const String verifyAgeScreenDescriptionBody =
+      'verifyAgeScreenDescriptionBody';
+  static const String creditsLabel = 'creditsLabel';
+  static const String fullyTransparentLabel = 'fullyTransprentLabel';
+  static const String licensesLabel = 'licensesLabel';
+  static const String notificationLabel = 'notificationLabel';
+  static const String advancedSettingsLabel = 'advancedSettingsLabel';
+  static const String settingsLabel = 'settingsLabel';
+  static const String nextLabel = 'nextLabel';
+  static const String onboardingLabel = 'onboardingLabel';
+  static const String dismissLabel = 'dismissLabel';
+  static const String signUpLabel = 'signUpLabel';
+  static const String privacyPolicyLabel = 'privacyPolicyLabel';
+  static const String privacyLabel = 'privacyLabel';
+  static const String privateLabel = 'privateLabel';
+  static const String offlineLabel = 'offlineLabel';
+  static const String agreeLabel = 'agreeLabel';
+  static const String deleteLabel = 'deleteLabel';
+  static const String deleteDescriptionBodyText = 'deleteDescriptionBodyText';
+  static const String deleteObjectTitle = 'deleteObjectTitle';
+  static const String confirmDeleteTitle = 'confirmDeleteTitle';
+  static const String deleteObjectPrompt = 'deleteObjectPrompt';
+}
