59bb4149f8351b7e63d8ef819d1c209f224ac915
eightgran <eightgran@gmail.com>
2021-12-07 20:42:51 +0100

    Add implementations for `de` and `en` locale - Seperated from LeitmotifLocalizations

---
+++ lib/src/localization/languages/leitmotif_localizations_de.dart
@@ -0,0 +1,87 @@
+import 'package:leitmotif/leitmotif.dart';
+
+/// The `de` language implementation of the [LeitmotifLocalizations].
+class LeitmotifLocalizationsDe {
+  /// The language code of German locales.
+  static const languageCode = 'de';
+
+  /// The localized values.
+  static const values = const {
+    LeitmotifLocalizationsKeys.submitLabel: 'Bestätigen',
+    LeitmotifLocalizationsKeys.applyLabel: 'Anwenden',
+    LeitmotifLocalizationsKeys.cancelLabel: 'Abbrechen',
+    LeitmotifLocalizationsKeys.startLabel: 'Start',
+    LeitmotifLocalizationsKeys.selectLabel: 'Auswählen',
+    LeitmotifLocalizationsKeys.resetLabel: 'Zurücksetzen',
+    LeitmotifLocalizationsKeys.dateInputLabel: 'Datum eingeben',
+    LeitmotifLocalizationsKeys.colorInputLabel: 'Enter Color',
+    LeitmotifLocalizationsKeys.ageInputLabel: 'Gib dein Alter ein',
+    LeitmotifLocalizationsKeys.ageRequiredLabel:
+        'Diese App benötigt dein Alter',
+    LeitmotifLocalizationsKeys.dayOfWeek1: 'Montag',
+    LeitmotifLocalizationsKeys.dayOfWeek2: 'Dienstag',
+    LeitmotifLocalizationsKeys.dayOfWeek3: 'Mittwoch',
+    LeitmotifLocalizationsKeys.dayOfWeek4: 'Donnerstag',
+    LeitmotifLocalizationsKeys.dayOfWeek5: 'Freitag',
+    LeitmotifLocalizationsKeys.dayOfWeek6: 'Samstag',
+    LeitmotifLocalizationsKeys.dayOfWeek7: 'Sonntag',
+    LeitmotifLocalizationsKeys.january: 'Januar',
+    LeitmotifLocalizationsKeys.february: 'Februar',
+    LeitmotifLocalizationsKeys.march: 'März',
+    LeitmotifLocalizationsKeys.april: 'April',
+    LeitmotifLocalizationsKeys.may: 'Mai',
+    LeitmotifLocalizationsKeys.june: 'Juni',
+    LeitmotifLocalizationsKeys.july: 'Juli',
+    LeitmotifLocalizationsKeys.august: 'August',
+    LeitmotifLocalizationsKeys.september: 'September',
+    LeitmotifLocalizationsKeys.october: 'Oktober',
+    LeitmotifLocalizationsKeys.november: 'November',
+    LeitmotifLocalizationsKeys.december: 'Dezember',
+    LeitmotifLocalizationsKeys.yourAgeLabel: 'Dein Alter',
+    LeitmotifLocalizationsKeys.invalidInputLabel: 'Fehlerhafte Eingaben',
+    LeitmotifLocalizationsKeys.invalidInputBody:
+        'Bitte überprüfe deine Eingabe.',
+    LeitmotifLocalizationsKeys.invalidInputExtendedBody:
+        'Einige deiner eingegebenen Werte sind fehlerhaft.' +
+            ' ' +
+            'Bitte überprüfe deine Eingabe.',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessTitle: 'Alter bestätigt',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessSubtitle:
+        'Dein Alter wurde bestätigt',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessBody:
+        'Vielen Dank für die Altersbestätigung. Viel Vergnügen!',
+    LeitmotifLocalizationsKeys.verifyAgeScreenDescriptionBody:
+        "Dein Alter hilft uns Funktionen dieser App zu gewährleisten." +
+            " " +
+            "Diese App wird keine Daten weitergeben." +
+            " " +
+            "Dein Alter wird geteilt.",
+    LeitmotifLocalizationsKeys.creditsLabel: 'Mitwirkende',
+    LeitmotifLocalizationsKeys.fullyTransparentLabel:
+        'Diese Farbe ist vollständig transparent',
+    LeitmotifLocalizationsKeys.licensesLabel: 'Lizenzen',
+    LeitmotifLocalizationsKeys.backLabel: 'Zurück',
+    LeitmotifLocalizationsKeys.notificationLabel: 'Benachrichtigung',
+    LeitmotifLocalizationsKeys.advancedSettingsLabel:
+        'Erweiterte Einstellungen',
+    LeitmotifLocalizationsKeys.settingsLabel: 'Einstellungen',
+    LeitmotifLocalizationsKeys.nextLabel: 'Nächste',
+    LeitmotifLocalizationsKeys.onboardingLabel: 'Start',
+    LeitmotifLocalizationsKeys.dismissLabel: 'Ausblenden',
+    LeitmotifLocalizationsKeys.signUpLabel: 'Registrieren',
+    LeitmotifLocalizationsKeys.privacyPolicyLabel: 'Datenschutzrichtlinien',
+    LeitmotifLocalizationsKeys.privacyLabel: 'Privatsphäre',
+    LeitmotifLocalizationsKeys.privateLabel: 'Vertraulich',
+    LeitmotifLocalizationsKeys.offlineLabel: 'Offline',
+    LeitmotifLocalizationsKeys.agreeLabel: 'Akzeptieren',
+    LeitmotifLocalizationsKeys.deleteLabel: 'Entfernen',
+    LeitmotifLocalizationsKeys.deleteDescriptionBodyText:
+        'Nach dem Löschen dieses Objekts, wird es nicht länger'
+            ' '
+            'verfügbar sein.',
+    LeitmotifLocalizationsKeys.deleteObjectTitle: 'Objekt löschen',
+    LeitmotifLocalizationsKeys.confirmDeleteTitle: 'Löschen bestätigen',
+    LeitmotifLocalizationsKeys.deleteObjectPrompt:
+        'Möchtest du dieses Objekt löschen?',
+  };
+}
---
+++ lib/src/localization/languages/leitmotif_localizations_en.dart
@@ -0,0 +1,84 @@
+import 'package:leitmotif/localization.dart';
+
+/// The `en` language implementation of the [LeitmotifLocalizations].
+class LeitmotifLocalizationsEn {
+  /// The language code of English locales.
+  static const languageCode = 'en';
+
+  /// The localized values.
+  static const values = const {
+    LeitmotifLocalizationsKeys.submitLabel: 'Submit',
+    LeitmotifLocalizationsKeys.applyLabel: 'Apply',
+    LeitmotifLocalizationsKeys.cancelLabel: 'Cancel',
+    LeitmotifLocalizationsKeys.startLabel: 'Start',
+    LeitmotifLocalizationsKeys.selectLabel: 'Select',
+    LeitmotifLocalizationsKeys.resetLabel: 'Reset',
+    LeitmotifLocalizationsKeys.dateInputLabel: 'Enter Date',
+    LeitmotifLocalizationsKeys.colorInputLabel: 'Enter Color',
+    LeitmotifLocalizationsKeys.ageInputLabel: 'Enter your Age',
+    LeitmotifLocalizationsKeys.ageRequiredLabel: 'This app does need your age',
+    LeitmotifLocalizationsKeys.dayOfWeek1: 'Monday',
+    LeitmotifLocalizationsKeys.dayOfWeek2: 'Tuesday',
+    LeitmotifLocalizationsKeys.dayOfWeek3: 'Wednesday',
+    LeitmotifLocalizationsKeys.dayOfWeek4: 'Thursday',
+    LeitmotifLocalizationsKeys.dayOfWeek5: 'Friday',
+    LeitmotifLocalizationsKeys.dayOfWeek6: 'Saturday',
+    LeitmotifLocalizationsKeys.dayOfWeek7: 'Sunday',
+    LeitmotifLocalizationsKeys.january: 'January',
+    LeitmotifLocalizationsKeys.february: 'February',
+    LeitmotifLocalizationsKeys.march: 'March',
+    LeitmotifLocalizationsKeys.april: 'April',
+    LeitmotifLocalizationsKeys.may: 'May',
+    LeitmotifLocalizationsKeys.june: 'June',
+    LeitmotifLocalizationsKeys.july: 'July',
+    LeitmotifLocalizationsKeys.august: 'August',
+    LeitmotifLocalizationsKeys.september: 'September',
+    LeitmotifLocalizationsKeys.october: 'October',
+    LeitmotifLocalizationsKeys.november: 'November',
+    LeitmotifLocalizationsKeys.december: 'December',
+    LeitmotifLocalizationsKeys.yourAgeLabel: 'Your Age',
+    LeitmotifLocalizationsKeys.invalidInputLabel: 'Invalid Input',
+    LeitmotifLocalizationsKeys.invalidInputBody: 'Please check your input.',
+    LeitmotifLocalizationsKeys.invalidInputExtendedBody:
+        'Some of your inputted values are not valid.'
+            ' '
+            'Please check your input.'
+            ''
+            'Please check your input.',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessTitle: 'Age confirmed',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessSubtitle:
+        'Your age has been confirmed',
+    LeitmotifLocalizationsKeys.verifyAgeScreenSuccessBody:
+        'Thank you for verifying your age. Have a good time!',
+    LeitmotifLocalizationsKeys.verifyAgeScreenDescriptionBody:
+        "Your date of birth helps us to enable certain functions" +
+            " " +
+            "on this app. We won't send any data to" +
+            " " +
+            "anyone. Your age will not be shared.",
+    LeitmotifLocalizationsKeys.creditsLabel: 'Credits',
+    LeitmotifLocalizationsKeys.fullyTransparentLabel:
+        'Color is fully transparent',
+    LeitmotifLocalizationsKeys.licensesLabel: 'Licenses',
+    LeitmotifLocalizationsKeys.backLabel: 'Back',
+    LeitmotifLocalizationsKeys.notificationLabel: 'Notification',
+    LeitmotifLocalizationsKeys.advancedSettingsLabel: 'Advanced Settings',
+    LeitmotifLocalizationsKeys.settingsLabel: 'Settings',
+    LeitmotifLocalizationsKeys.nextLabel: 'Next',
+    LeitmotifLocalizationsKeys.onboardingLabel: 'Onboarding',
+    LeitmotifLocalizationsKeys.dismissLabel: 'Dismiss',
+    LeitmotifLocalizationsKeys.signUpLabel: 'Sign Up',
+    LeitmotifLocalizationsKeys.privacyPolicyLabel: 'Privacy Policy',
+    LeitmotifLocalizationsKeys.privacyLabel: 'Privacy',
+    LeitmotifLocalizationsKeys.privateLabel: 'Private',
+    LeitmotifLocalizationsKeys.offlineLabel: 'Offline',
+    LeitmotifLocalizationsKeys.agreeLabel: 'Agree',
+    LeitmotifLocalizationsKeys.deleteLabel: 'Delete',
+    LeitmotifLocalizationsKeys.deleteDescriptionBodyText:
+        'By deleting this object, it will no longer be available.',
+    LeitmotifLocalizationsKeys.deleteObjectTitle: 'Delete Object',
+    LeitmotifLocalizationsKeys.confirmDeleteTitle: 'Confirm Delete',
+    LeitmotifLocalizationsKeys.deleteObjectPrompt:
+        'Do you want to delete this object?',
+  };
+}
