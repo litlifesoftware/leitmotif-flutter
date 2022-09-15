391104bbbb1451ec690c007853b24db694266d6c
eightgran <eightgran@gmail.com>
2022-09-15 20:22:17 +0200

    Update `LitIcons` - Add new Icons - Remove LitIconData class

--- lib/src/icons/lit_icons.dart
+++ lib/src/icons/lit_icons.dart
@@ -1,82 +1,101 @@
 import 'package:flutter/widgets.dart';
 
-import 'lit_icon_data.dart';
-
 /// The IconData objects used to implement the LitIcons.
 
 class LitIcons {
   LitIcons._();
-  static const IconData chevron_right = LitIconData(0xe800);
-  static const IconData chevron_right_solid = LitIconData(0xe801);
-  static const IconData chevron_left = LitIconData(0xe802);
-  static const IconData chevron_left_solid = LitIconData(0xe803);
-  static const IconData diary = LitIconData(0xe804);
-  static const IconData diary_alt = LitIconData(0xe805);
-  static const IconData disk = LitIconData(0xe806);
-  static const IconData disk_alt = LitIconData(0xe807);
-  static const IconData gear = LitIconData(0xe808);
-  static const IconData gear_solid = LitIconData(0xe809);
-  static const IconData heart = LitIconData(0xe80a);
-  static const IconData heart_solid = LitIconData(0xe80b);
-  static const IconData home = LitIconData(0xe80c);
-  static const IconData home_alt = LitIconData(0xe80d);
-  static const IconData info = LitIconData(0xe80e);
-  static const IconData moon = LitIconData(0xe80f);
-  static const IconData moon_solid = LitIconData(0xe810);
-  static const IconData moon_with_stars = LitIconData(0xe811);
-  static const IconData moon_with_stars_solid = LitIconData(0xe812);
-  static const IconData plus = LitIconData(0xe813);
-  static const IconData times = LitIconData(0xe814);
-  static const IconData animation = LitIconData(0xe815);
-  static const IconData arrow_right = LitIconData(0xe816);
-  static const IconData arrow_right_solid = LitIconData(0xe817);
-  static const IconData arrow_left = LitIconData(0xe818);
-  static const IconData arrow_left_solid = LitIconData(0xe819);
-  static const IconData check = LitIconData(0xe81a);
-  static const IconData rgb_solid = LitIconData(0xe81b);
-  static const IconData pencil = LitIconData(0xe81c);
-  static const IconData pencil_alt = LitIconData(0xe81d);
-  static const IconData mix = LitIconData(0xe81e);
-  static const IconData rgb = LitIconData(0xe81f);
-  static const IconData photos = LitIconData(0xe820);
-  static const IconData map_marker = LitIconData(0xe821);
-  static const IconData person_solid = LitIconData(0xe822);
-  static const IconData person = LitIconData(0xe823);
-  static const IconData quote_right = LitIconData(0xe824);
-  static const IconData quote_left = LitIconData(0xe825);
-  static const IconData bolt = LitIconData(0xe827);
-  // static const IconData document_solid = LitIconData(0xe800);
-  // static const IconData disk = LitIconData(0xe801);
-  // static const IconData disk_solid = LitIconData(0xe802);
-  // static const IconData diary = LitIconData(0xe803);
-  // static const IconData diary_solid = LitIconData(0xe804);
-  // static const IconData chevron_right_solid = LitIconData(0xe805);
-  // static const IconData chevron_right = LitIconData(0xe806);
-  // static const IconData check = LitIconData(0xe807);
-  // static const IconData check_circle = LitIconData(0xe808);
-  // static const IconData check_solid = LitIconData(0xe809);
-  // static const IconData arrow_right_solid = LitIconData(0xe80a);
-  // static const IconData arrow_right = LitIconData(0xe80b);
-  // static const IconData arrow_left_solid = LitIconData(0xe80c);
-  // static const IconData animation = LitIconData(0xe80d);
-  // static const IconData arrow_left = LitIconData(0xe80e);
-  // static const IconData moon = LitIconData(0xe80f);
-  // static const IconData moon_with_stars = LitIconData(0xe810);
-  // static const IconData moon_solid = LitIconData(0xe811);
-  // static const IconData moon_with_stars_solid = LitIconData(0xe812);
-  // static const IconData chevron_left = LitIconData(0xe813);
-  // static const IconData chevron_left_solid = LitIconData(0xe814);
-  // static const IconData times = LitIconData(0xe815);
-  // static const IconData times_solid = LitIconData(0xe816);
-  // static const IconData plus = LitIconData(0xe817);
-  // static const IconData plus_solid = LitIconData(0xe818);
-  // static const IconData minus = LitIconData(0xe819);
-  // static const IconData minus_solid = LitIconData(0xe81a);
-  // static const IconData heart = LitIconData(0xe81b);
-  // static const IconData info = LitIconData(0xe81c);
-  // static const IconData gear_solid = LitIconData(0xe81d);
-  // static const IconData gear = LitIconData(0xe81e);
-  // static const IconData document = LitIconData(0xe81f);
-  // static const IconData heart_solid = LitIconData(0xe820);
-  // static const IconData info_solid = LitIconData(0xe821);
+
+  static const _kFontFam = 'LitIcons';
+  static const String? _kFontPkg = 'leitmotif';
+
+  static const IconData animation =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_left =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_left_solid =
+      IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_right =
+      IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData arrow_right_solid =
+      IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData bolt =
+      IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData check =
+      IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chevron_left =
+      IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chevron_left_solid =
+      IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chevron_right =
+      IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData diary =
+      IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData chevron_right_solid =
+      IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData diary_alt =
+      IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData disk =
+      IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData disk_alt =
+      IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData document =
+      IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData document_alt =
+      IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData folder =
+      IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData folder_solid =
+      IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData gear =
+      IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData gear_alt =
+      IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heart =
+      IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heart_solid =
+      IconData(0xe816, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home =
+      IconData(0xe817, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData home_alt =
+      IconData(0xe818, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData llsw_smiley =
+      IconData(0xe819, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData info =
+      IconData(0xe81a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moon =
+      IconData(0xe81b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData map_marker =
+      IconData(0xe81c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moon_solid =
+      IconData(0xe81d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moon_with_stars =
+      IconData(0xe81e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData moon_with_stars_solid =
+      IconData(0xe81f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pencil =
+      IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData pencil_alt =
+      IconData(0xe821, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person =
+      IconData(0xe822, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData person_solid =
+      IconData(0xe823, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData photo =
+      IconData(0xe824, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData photos =
+      IconData(0xe825, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData plus =
+      IconData(0xe826, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData quote_left =
+      IconData(0xe827, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData quote_right =
+      IconData(0xe828, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rgb =
+      IconData(0xe829, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData times =
+      IconData(0xe82a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData rgb_solid =
+      IconData(0xe82b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData trash_bin =
+      IconData(0xe82c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
