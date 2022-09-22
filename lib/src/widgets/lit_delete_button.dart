85e0934e146c99a2f372d12f4ab8509fde90ed9e
eightgran <eightgran@gmail.com>
2022-09-22 22:45:23 +0200

    Add styling options on `LitDeleteButton` - Make uppercase text optional - Show optional icon

--- lib/src/widgets/lit_delete_button.dart
+++ lib/src/widgets/lit_delete_button.dart
@@ -4,12 +4,24 @@ import 'package:leitmotif/leitmotif.dart';
 /// A Leitmotif `buttons` widget allowing to delete an object by pressing a
 /// button.
 class LitDeleteButton extends StatelessWidget {
+  final TextAlign textAlign;
+
+  final TextStyle textStyle;
+
+  final bool upperCase;
+
+  final bool showIcon;
+
   /// Handles the `onPressed` action.
   final void Function() onPressed;
 
   /// Creates a [LitDeleteButton].
   const LitDeleteButton({
     Key? key,
+    this.textAlign = TextAlign.left,
+    this.textStyle = LitSansSerifStyles.button,
+    this.upperCase = true,
+    this.showIcon = false,
     required this.onPressed,
   }) : super(key: key);
 
@@ -19,16 +31,70 @@ class LitDeleteButton extends StatelessWidget {
   /// The button's background color.
   static const backgroundColor = LitColors.red200;
 
+  MainAxisAlignment get _rowAlignment {
+    switch (textAlign) {
+      case TextAlign.center:
+        return MainAxisAlignment.center;
+      case TextAlign.left:
+        return MainAxisAlignment.start;
+      case TextAlign.end:
+        return MainAxisAlignment.end;
+      default:
+        return MainAxisAlignment.start;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return LitPushedThroughButton(
       accentColor: accentColor,
       backgroundColor: backgroundColor,
-      child: ClippedText(
-        LeitmotifLocalizations.of(context).deleteLabel.toUpperCase(),
-        style: LitSansSerifStyles.button,
-      ),
+      child: showIcon
+          ? Row(
+              mainAxisAlignment: _rowAlignment,
+              children: [
+                Icon(
+                  LitIcons.trash_bin,
+                  color: textStyle.color,
+                  size: textStyle.fontSize,
+                ),
+                SizedBox(width: 8.0),
+                ClippedText(
+                  LeitmotifLocalizations.of(context).deleteLabel,
+                  style: textStyle,
+                  textAlign: textAlign,
+                  upperCase: upperCase,
+                ),
+              ],
+            )
+          : _Text(
+              textAlign: textAlign,
+              textStyle: textStyle,
+              upperCase: upperCase,
+            ),
       onPressed: onPressed,
+    );
+  }
+}
+
+class _Text extends StatelessWidget {
+  final TextAlign textAlign;
+  final TextStyle textStyle;
+  final bool upperCase;
+  const _Text({
+    Key? key,
+    required this.textAlign,
+    required this.textStyle,
+    required this.upperCase,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClippedText(
+      LeitmotifLocalizations.of(context).deleteLabel,
+      style: textStyle,
+      textAlign: textAlign,
+      upperCase: upperCase,
     );
   }
 }
